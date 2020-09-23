b1d4026b45803bc81d53d5903f11b28608086b74
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-09-23 15:02:03 +0600

    begin

--- DsStan/Program.cs
+++ DsStan/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DsStan
@@ -10,6 +11,17 @@ namespace DsStan
     {
         static void Main(string[] args)
         {
+
+            clStan stan = new clStan();
+            Thread task1 = new Thread(stan);
+            task1.Start();
+
+
+            clDs Ds = new clDs();
+            Thread task2 = new Thread(Ds);
+            task2.Start();
+
+
         }
     }
 }
